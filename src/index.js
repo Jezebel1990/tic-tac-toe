daedc2c8c36bfcfc1dc224de0a7ed0bb3012b262
Jezebel1990 <jezebelguedes@gmail.com>
2023-02-19 23:48:41 -0300

    create components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import  ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const Square = () => {
+  return (
+    <div style={{
+      backgroundColor: 'gold',
+      margin: 10,
+      padding: 20,
+    }}>
+   Square
+    </div>
+  )
+}
+
+
+
+
+
+const Board = () => {
+  return (
+    <div style={{
+      backgroundColor: 'skyblue',
+      margin: 10,
+      padding: 20,
+    }}>
+      Board
+<Square />
+    </div>
+  );
+};
+
+
+
+const Game = () => {
+  return (
+<div style={{
+  backgroundColor: 'salmon',
+  margin: 10,
+  padding: 20,
+
+}}>
+Game
+<Board/>
+</div>
+
+  );
+};
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
